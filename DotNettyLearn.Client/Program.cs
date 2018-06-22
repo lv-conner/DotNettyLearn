8993205608f29dd340e837aa4f92067377ed63f9
lv-conner <lvmh123456@gmail.com>
2018-06-22 16:34:26 +0800

    TCPServerSocketChannel错误

---
+++ DotNettyLearn.Client/Program.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Echo.Client
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DotNetty.Buffers;
+    using DotNetty.Codecs;
+    using DotNetty.Handlers.Logging;
+    using DotNetty.Handlers.Tls;
+    using DotNetty.Transport.Bootstrapping;
+    using DotNetty.Transport.Channels;
+    using DotNetty.Transport.Channels.Sockets;
+
+    class Program
+    {
+        static async Task RunClientAsync()
+        {
+
+            var group = new MultithreadEventLoopGroup();
+
+            X509Certificate2 cert = null;
+            string targetHost = null;
+            if (true)
+            {
+                cert = new X509Certificate2(Path.Combine(Directory.GetCurrentDirectory(), "dotnetty.com.pfx"), "password");
+                targetHost = cert.GetNameInfo(X509NameType.DnsName, false);
+            }
+            try
+            {
+                var bootstrap = new Bootstrap();
+                bootstrap
+                    .Group(group)
+                    .Channel<TcpSocketChannel>()
+                    .Option(ChannelOption.TcpNodelay, true)
+                    .Handler(new ActionChannelInitializer<ISocketChannel>(channel =>
+                    {
+                        IChannelPipeline pipeline = channel.Pipeline;
+
+                        //if (cert != null)
+                        //{
+                        //    pipeline.AddLast("tls", new TlsHandler(stream => new SslStream(stream, true, (sender, certificate, chain, errors) => true), new ClientTlsSettings(targetHost)));
+                        //}
+                        pipeline.AddLast(new LoggingHandler());
+                        pipeline.AddLast("framing-enc", new LengthFieldPrepender(2));
+                        pipeline.AddLast("framing-dec", new LengthFieldBasedFrameDecoder(ushort.MaxValue, 0, 2, 0, 2));
+
+                        pipeline.AddLast("echo", new EchoClientHandler());
+                    }));
+
+                IChannel clientChannel = await bootstrap.ConnectAsync(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8007));
+                Console.ReadLine();
+
+                await clientChannel.CloseAsync();
+            }
+            finally
+            {
+                await group.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1));
+            }
+        }
+
+        static void Main() => RunClientAsync().Wait();
+    }
+
+    public class EchoClientHandler : ChannelHandlerAdapter
+    {
+        readonly IByteBuffer initialMessage;
+
+        public EchoClientHandler()
+        {
+            this.initialMessage = Unpooled.Buffer(8192);
+            byte[] messageBytes = Encoding.UTF8.GetBytes("Hello world");
+            this.initialMessage.WriteBytes(messageBytes);
+        }
+
+        public override void ChannelActive(IChannelHandlerContext context) => context.WriteAndFlushAsync(this.initialMessage);
+
+        public override void ChannelRead(IChannelHandlerContext context, object message)
+        {
+            var byteBuffer = message as IByteBuffer;
+            if (byteBuffer != null)
+            {
+                Console.WriteLine("Received from server: " + byteBuffer.ToString(Encoding.UTF8));
+            }
+            context.WriteAsync(message);
+        }
+
+        public override void ChannelReadComplete(IChannelHandlerContext context) => context.Flush();
+
+        public override void ExceptionCaught(IChannelHandlerContext context, Exception exception)
+        {
+            Console.WriteLine("Exception: " + exception);
+            context.CloseAsync();
+        }
+    }
+}
